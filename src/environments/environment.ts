f68d3ddcc074d69213100f85caf4649af597109a
ajjnorton <ajjnorton@icloud.com>
2023-01-27 11:33:25 +0000

    stash

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'rugbyrec-app',
+    appId: '1:726775817809:web:3cf939535b37f3a5c87a70',
+    databaseURL: 'https://rugbyrec-app.firebaseio.com',
+    storageBucket: 'rugbyrec-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCPZE2Y6Al0dn0NOB0NMNyD5Y60SkugNF4',
+    authDomain: 'rugbyrec-app.firebaseapp.com',
+    messagingSenderId: '726775817809',
+  }
+};
