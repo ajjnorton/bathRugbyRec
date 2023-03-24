9ed2d92eb24e080ad81806161b3826af337c4d24
ajjnorton <ajjnorton@icloud.com>
2023-03-24 09:29:41 +0000

    latest

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect').default
 
 const app = express();
+
+
+app.use(sslRedirect(['production'], 301));
 
 app.use(express.static('./dist/bath-rugby-rec'));
 
@@ -9,4 +13,10 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/bath-rugby-rec/index.html'))
 });
 
-app.listen(process.env.PORT || 8080)
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+
+})
