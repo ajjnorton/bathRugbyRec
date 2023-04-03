ca196ef53153cdd2b67519174278f1e92dca6d23
Anthony Norton <anthony@modularity.io>
2023-04-03 10:23:22 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: false,
   firebase: {
     projectId: 'rugbyrec-app',
-    appId: '1:726775817809:web:3cf939535b37f3a5c87a70',
-    databaseURL: 'https://rugbyrec-app.firebaseio.com',
+    appId: '',
+    databaseURL: '',
     storageBucket: 'rugbyrec-app.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyCPZE2Y6Al0dn0NOB0NMNyD5Y60SkugNF4',
-    authDomain: 'rugbyrec-app.firebaseapp.com',
-    messagingSenderId: '726775817809',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   }
 };
